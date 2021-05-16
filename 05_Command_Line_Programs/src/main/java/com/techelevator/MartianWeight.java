7bb71a9d0375832dffd4b2751186288c0045b442
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-05-16 10:40:13 -0400

    Pair HW submission

--- 05_Command_Line_Programs/src/main/java/com/techelevator/MartianWeight.java
+++ 05_Command_Line_Programs/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -19,6 +21,41 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter a series of Earth weights separated with a space: ");
+
+		String userEarthWeights = input.nextLine();
+
+		//double userStringToDoubleConversion =  Double.parseDouble(userEarthWeights);
+
+		String earthWeightsStringArray[] = userEarthWeights.split(" ");
+
+		int earthWeightsIntegerArray[] = new int[earthWeightsStringArray.length];
+
+		for(int i = 0 ; i < earthWeightsStringArray.length ; i++) {
+
+			earthWeightsIntegerArray[i] = Integer.parseInt(earthWeightsStringArray[i]);
+		}
+		//For Loop to run an
+
+		//System.out.println(earthWeightsIntegerArray[3]);
+
+		for( int i = 0 ; i < earthWeightsIntegerArray.length ; i++ ) {
+			double martianWeight = earthWeightsIntegerArray[i] * 0.378;
+			System.out.println(earthWeightsIntegerArray[i] + " lbs. on Earth, is " + martianWeight + " lbs. on Mars.");
+		}
+		//System.out.println("What is the height of the container in feet: ");
+		//String userHeightOfContainer = input.nextLine();
+		//double heightOfContainer = Double.parseDouble( userHeightOfContainer );
+
+
+
+
+
+
+
 
 	}
 
